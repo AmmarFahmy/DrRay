68195423762c58f7db037766cf2f229c8afbedc8
Ammar_Fahmy <40536975+AmmarFahmy@users.noreply.github.com>
2020-09-04 01:19:18 +0530

    Completed

--- src/java/controllers/changedetails.java
+++ src/java/controllers/changedetails.java
@@ -8,18 +8,13 @@ package controllers;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author ASUS
- */
+
 @WebServlet(name = "changedetails", urlPatterns = {"/changedetails"})
 public class changedetails extends HttpServlet {
 
