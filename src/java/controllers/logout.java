86fb990313e9081198b54262d6b0ebe790ea090e
Ammar_Fahmy <40536975+AmmarFahmy@users.noreply.github.com>
2020-09-04 01:19:07 +0530

    Completed

--- src/java/controllers/logout.java
+++ src/java/controllers/logout.java
@@ -14,10 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author ISK
- */
+
 @WebServlet(name = "logout", urlPatterns = {"/logout"})
 public class logout extends HttpServlet {
 
